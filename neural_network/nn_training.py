cfc02d1496f36e0e7183e395193fecdbd6aee2fe
Bill Tran <trantriducs@gmail.com>
2022-04-26 16:53:55 -0400

    feat: update debugged neural net training code

--- neural_network/nn_training.py
+++ neural_network/nn_training.py
@@ -17,18 +17,17 @@ def convert_angle(angle):
 
 
 def AI_loop():
+    ai.thrust(0)
+    ai.turnLeft(0)
+    ai.turnRight(0)
     ai.setTurnSpeed(20)
     ai.setPower(35)
-    ai.turnRight(0)
-    ai.turnLeft(0)
-    ai.thrust(0)
 
-    heading = convert_angle(int(ai.selfHeadingDeg()))
-
+    heading = int(ai.selfHeadingDeg())
     vel = ai.selfSpeed() / 15
     reload_time = int(ai.selfReload()) / 12
 
-    N = ai.wallFeeler(1000, heading) / 1000
+    N = ai.wallFeeler(1000, heading) /1000
     S = ai.wallFeeler(1000, heading - 180) / 1000
     W = ai.wallFeeler(1000, heading - 90) / 1000
     E = ai.wallFeeler(1000, heading + 90) / 1000
@@ -38,8 +37,8 @@ def AI_loop():
     SE = ai.wallFeeler(1000, heading - 135) / 1000
 
     enemy_dist = ai.enemyDistance(0) / 1000
-    enemy_dir = convert_angle(ai.aimdir(0))
-    enemy_heading = convert_angle(ai.enemyHeadingDeg(0))
+    enemy_dir = convert_angle(ai.aimdir(0)) or 0
+    enemy_heading = convert_angle(ai.enemyHeadingDeg(0)) or 0
 
     enemy_reload_time = ai.enemyReload(0) / 12
     bullet_dist = ai.shotDist(0) / 1000
@@ -54,7 +53,7 @@ def AI_loop():
     mdb_angle = convert_angle((math.atan2(delta_y, delta_x) * 180 / math.pi) + 180)  # 0-360 with +180
 
     bias = 1
-    data = [heading, vel, reload_time, N, S, W, E, NW, NE, SW, SE, enemy_dist,
+    data = [convert_angle(heading), vel, reload_time, N, S, W, E, NW, NE, SW, SE, enemy_dist,
             enemy_dir, enemy_heading, enemy_reload_time, bullet_dist, mdb_angle, bias]
 
     global survival_time
@@ -69,30 +68,36 @@ def AI_loop():
         weights_updated = False
 
     if ai.selfAlive() == 0 and weights_updated is False and row_count < 514:
-        with open('nn_population.csv', newline='') as f:
+        
+        with open('nn_population.csv', 'r') as f:
             r = csv.reader(f)
             lines = list(r)
             lines[row_count - 1][1] = str(survival_time)
+
+        with open('nn_population.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerows(lines)
 
-            chromosome = lines[row_count - 1][0]
-            weights = [
-                convert_genes_to_weight(chromosome[i:i + 6])
-                for i in range(0, len(chromosome), 6)
-            ]
+        chromosome = lines[row_count][0]
+        weights = [
+            convert_genes_to_weight(chromosome[i:i + 6])
+            for i in range(0, len(chromosome), 6)
+        ]
+
+        
 
         row_count += 1
         weights_updated = True
         survival_time = 0
 
-    elif ai.selfAlive() == 0 and weights_updated is False and row_count == 514:
+    elif ai.selfAlive() == 0 and weights_updated is False and row_count == 52:
         evolve_one_generation('nn_population.csv', 'nn_ga_config.json')
         weights = get_initial_weights()
         row_count = 2
         weights_updated = True
         survival_time = 0
 
+ 
     thrust = sum([i*j for i, j in zip(data, weights[:18])])
     if thrust > 0:
         ai.thrust(1)
@@ -100,7 +105,6 @@ def AI_loop():
     shoot = sum([i*j for i, j in zip(data, weights[18:36])])
     if shoot > 0:
         ai.fireShot()
-
     turn = sum([i*j for i, j in zip(data, weights[37:])]) / 15
     if turn > 1:
         turn = 1
@@ -109,14 +113,15 @@ def AI_loop():
     ai.turn(int(turn * 20))
 
     if ai.selfScore() > game_score:
-        game_score = ai.selfScore()
-        survival_time += 20
+       game_score = ai.selfScore()
+       survival_time += 20
 
 
 def get_initial_weights():
     with open('nn_population.csv', newline='') as f:
         csv_reader = csv.reader(f)
-        line = next(next(csv_reader))
+        next(csv_reader)
+        line = next(csv_reader)
         return [
             convert_genes_to_weight(line[0][i:i + 6])
             for i in range(0, len(line[0]), 6)
@@ -129,5 +134,6 @@ if __name__ == "__main__":
     row_count = 2
     weights = get_initial_weights()
     game_score = 0
+
 
     ai.start(AI_loop, ["-name", "NNBot", "-join", "localhost"])
